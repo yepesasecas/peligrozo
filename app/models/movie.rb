c4711dd4a783708be1fe6d8b394c156b357cbd0e
@yepesasecas <yepes07@gmail.com>
2014-07-25 23:23:41 -0500

    add overview and poster_path from TMDB

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -7,8 +7,10 @@ def get_movie_details
   Tmdb::Api.language("es")
   movie = Tmdb::Movie.search name
   if not movie.empty?
-    details = Tmdb::Movie.detail movie.first.id 
-    self.release_date = details.release_date
+    details = Tmdb::Movie.detail movie.first.id
+    self.overview     = details.overview if overview.nil?
+    self.poster_path  = details.poster_path if poster_path.nil?
+    self.release_date = details.release_date 
     self.tmdb_id      = details.id
   end
   puts "movie.rb file: #{self.name}"
