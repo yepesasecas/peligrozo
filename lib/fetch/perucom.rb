8f2a1377025328efff37a3133ba8449d6150183c
@yepesasecas <yepes07@gmail.com>
2014-07-27 16:03:52 -0500

    Module PErucom log

--- lib/fetch/perucom.rb
+++ lib/fetch/perucom.rb
@@ -81,15 +81,15 @@ module Fetch
           ul_div.children.each do |li|
             if li.class == Nokogiri::XML::Element
               p "li - #{li.class}"
-              p "li - #{li}"
+              p li
               figure_div = li.children[1]
               p "figure_div- #{figure_div.class}"
-              p "figure_div- #{figure_div}"
+              p figure_div
               a_div      = figure_div.children[1]
               p "a_div- #{a_div.class}"
-              p "a_div- #{a_div}"
+              p a_div
               img_div    = a_div.children[1]
-              p "img_div- #{img_div.class}"
+              p img_div
               if img_div
                 img_attributes = img_div.attributes 
                 poster_movie   = img_attributes["alt"].value
