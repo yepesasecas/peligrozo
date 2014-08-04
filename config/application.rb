2f5ec17ffe74e57fb05fbf03a112b7ddc2ba1552
@yepesasecas <yepes07@gmail.com>
2014-08-03 21:43:57 -0500

    add new font ebrima

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module Peligrozo
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.autoload_paths += %W(#{config.root}/lib)
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
   end
 end
