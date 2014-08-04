78f35a02d9cdf033328f961598cffd902f723608
@yepesasecas <yepes07@gmail.com>
2014-08-03 23:58:43 -0500

    modify runner to fetch

--- fetchMovies.rb
+++ fetchMovies.rb
@@ -1,16 +1,1 @@
-require 'open-uri'
-require 'nokogiri'
-require 'mechanize'
-
-
-p "fetchMovies.."
-
-movies = Fetch::Perucom.get_movies
-movies.each {|movie| movie.save}
-
-theaters = Fetch::Perucom.get_theaters
-theaters.each {|theater| theater.save}
-
-Fetch::Perucom.create_schedules(movies, theaters)
-
-p "FetchMovies.. DONE"
+MoviesFactory.fetch
