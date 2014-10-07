1e1a81a7a98ae63a0b8db7ede88bf774e28eac83
@yepesasecas <yepes07@gmail.com>
2014-10-07 11:19:09 -0500

    add counter to Movie Match

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+ 
   def bootstrap_class_for flash_type
     case flash_type
       when :success
@@ -13,4 +14,14 @@ module ApplicationHelper
         flash_type.to_s
     end
   end
+
+  def movie_match
+    count = Movie.with_no_trailer.count
+    if count > 0
+      "MOVIE MATCH (#{count})"
+    else
+      "MOVIE MATCH"
+    end
+  end
+  
 end
