6bb8487a1db7683d014727debaadcb07ad79a161
@yepesasecas <yepes07@gmail.com>
2014-10-11 12:21:02 -0500

    add movie_details as a Module

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ActiveRecord::Base
-  before_create :get_details, :get_trailer
+  include MovieDetails
+  
+  before_create :get_details
  
   has_many :schedules
   has_many :favorite_movies
@@ -22,38 +24,15 @@ class Movie < ActiveRecord::Base
   def self.upcoming
     upcomings = Fetch::Moviesdb.upcoming
     upcomings.map do |upcoming|
-      Movie.new name: upcoming["original_title"], 
-        release_date: upcoming["release_date"], 
-         poster_path: upcoming["poster_path"], 
-             tmdb_id: upcoming["tmdb_id"]
+      Movie.new( name:         upcoming["original_title"], 
+                 release_date: upcoming["release_date"], 
+                 poster_path:  upcoming["poster_path"], 
+                 tmdb_id:      upcoming["id"] )
     end
   end
 
-  private
-  
-    def get_details
-      p "get_details"
-      movie = Fetch::Moviesdb.search self.name
-      if not movie.empty?
-        details = Fetch::Moviesdb.detail movie.first.id
-        self.overview     = details.overview if overview.nil?
-        #self.poster_path  = details.poster_path if poster_path.nil?
-        self.release_date = details.release_date 
-        self.tmdb_id      = details.id
-      end
-    end
-
-    def get_trailer
-      p "get_trailer"
-      trailers = Fetch::Moviesdb.trailers self.tmdb_id
-      youtube  = trailers['youtube']
-      if not youtube.nil?
-        trailers = youtube.select {|video| video['type'] == 'Trailer'} 
-        if trailers.empty?
-          self.trailer = youtube[0]
-        else
-          self.trailer = trailers[0]["source"]
-        end
-      end
-    end
+  def details
+    get_details
+    self
+  end
 end
