ff34950fe08091d3b3f9516cc21e54bd08dcc83f
Andres Yepes <yepes07@gmail.com>
2014-11-12 09:28:41 -0500

    add genres and theaters integration tests

---
+++ test/integration/theaters_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class TheatersTest < ActionDispatch::IntegrationTest
+
+  def setup
+    DatabaseCleaner.clean
+    Capybara.current_driver = :selenium
+    visit root_path
+    
+    setup_omniauth_test(:config_done)
+    sign_in_with_facebook
+    click_link "dropdown"
+    click_link "TUS CINES"
+  end
+
+  test "As a user, I can change my favorite theaters" do 
+    assert has_content?("CINÉPOLIS SANTA ANITA"), "'Tus Cines' should have one theater."
+  end
+  
+end
