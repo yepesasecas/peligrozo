6ab4396a034f17b117b24188b055939b037650c3
Andres Yepes <yepes07@gmail.com>
2014-11-14 09:32:04 -0500

    refactor favorite_movies state to dont include eliminated movies

--- app/models/favorite_movie.rb
+++ app/models/favorite_movie.rb
@@ -3,14 +3,11 @@ class FavoriteMovie < ActiveRecord::Base
   belongs_to :movie
 
   state_machine :state, initial: :to_watch do
-    
+
     event :watch do
       transition from: :to_watch, to: :watched
     end
-    
-    event :eliminate do
-      transition from: :to_watch, to: :eliminated
-    end
+  
   end
 
 end
