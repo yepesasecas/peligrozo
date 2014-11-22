c12d5039a88a8218ebed35da8650f0120ee8a4ae
Andres Yepes <yepes07@gmail.com>
2014-11-22 16:25:30 -0500

    fix, remove eliminated movies form watchlist

--- app/controllers/eliminated_movies_controller.rb
+++ app/controllers/eliminated_movies_controller.rb
@@ -1,11 +1,18 @@
 class EliminatedMoviesController < ApplicationController
 
   def create
-    eliminate_movie = current_user.eliminated_movies.new(eliminated_movies_params)
-    if eliminate_movie.save
-      @movies   = MovieDecorator.build_with(Movie.playing_now_for(current_user))
-      @upcoming = MovieDecorator.build_with(Movie.upcoming_for(current_user))
+    favorite_movie = current_user.watchlist.find_by(movie_id: eliminated_movies_params[:movie_id])
+    unless favorite_movie.nil?
+      favorite_movie.destroy
     end
+
+    eliminated_movie = current_user.eliminated_movies.new(eliminated_movies_params)
+    if eliminated_movie.save
+      @movies    = MovieDecorator.build_with(Movie.playing_now_for(current_user))
+      @upcoming  = MovieDecorator.build_with(Movie.upcoming_for(current_user))
+      @watchlist = MovieDecorator.build_with(current_user.movies.in_watchlist)
+    end
+
   end
 
   private
