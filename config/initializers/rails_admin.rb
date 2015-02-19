b14e47932dbac1ed846d3fb8d345a531eee6abde
Andres Yepes <yepes07@gmail.com>
2015-02-19 10:12:40 -0500

    add Rails_Admin Tool

---
+++ config/initializers/rails_admin.rb
@@ -0,0 +1,24 @@
+RailsAdmin.config do |config|
+  
+  config.authorize_with do
+    authenticate_or_request_with_http_basic('Site Message') do |username, password|
+      username == ENV["ADMIN_USER"] && password == ENV["ADMIN_PASS"]
+    end
+  end
+
+  config.actions do
+    dashboard                     # mandatory
+    index                         # mandatory
+    new
+    export
+    bulk_delete
+    show
+    edit
+    delete
+    show_in_app
+
+    ## With an audit adapter, you can add:
+    # history_index
+    # history_show
+  end
+end
