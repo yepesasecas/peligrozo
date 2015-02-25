f74954f61d4c39a0a825739337bcf56f5ec242a3
Andres Yepes <yepes07@gmail.com>
2015-02-25 11:47:26 -0500

    rename admin controller to matches controller, and remove admin files

--- app/controllers/admins_controller.rb
+++
@@ -1,7 +0,0 @@
-class AdminsController < ApplicationController
-  before_action :user_logged_in?, :admin_logged_in?
-  
-  def index
-    @movies = Movie.with_no_trailer_or_overview.order('created_at DESC')
-  end
-end
