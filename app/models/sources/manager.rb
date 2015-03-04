214ce06f52f4ef1236c4871ac0034a525a872883
Andres Yepes <yepes07@gmail.com>
2015-03-04 14:10:28 -0500

    add Perucom and Colombiacom sources

--- app/models/sources/manager.rb
+++ app/models/sources/manager.rb
@@ -1,7 +1,7 @@
 module Sources
   class Manager
 
-    SOURCES = [Sources::Colombiacom]
+    SOURCES = [Sources::Perucom, Sources::Colombiacom]
 
     def self.fetch_movies(args ={})
       response = []
