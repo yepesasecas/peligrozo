006072c508f7fb8062d2bd17f3d348f7b0f6ae4c
luis <luisfelipe.guevaraa@gmail.com>
2015-04-08 12:23:58 -0500

    friendships

---
+++ app/assets/javascripts/invite_friends.js.coffee
@@ -0,0 +1,26 @@
+$(document).ready ->
+  path = window.location.pathname
+  $("body").css("background-color", "#eee") if path.search('invite_friends') != -1 and path.search('users') != -1
+  checkboxes = $(".checkbox.checkbox-friends").slice(10)
+  checkboxes.hide()
+
+  $("#more-friends").on "click", (e)->
+    e.preventDefault()
+    checkboxes = $(".checkbox.checkbox-friends").slice(15)
+    checkboxes.toggle()
+  
+  $("input[type='checkbox']").on "change", ->
+    guardar = $(this).is(":checked")
+    url =
+      if guardar
+        path
+      else
+        path + "/delete"
+    data =
+      "friend":
+        uid:this.value
+    $.ajax
+      type: "POST"
+      url: url
+      data: data
+      success: (response)-> console.log response
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150406164741) do
+ActiveRecord::Schema.define(version: 20150408162644) do
 
   create_table "cities", force: true do |t|
     t.string   "name"
@@ -78,7 +78,6 @@ ActiveRecord::Schema.define(version: 20150406164741) do
     t.integer  "movie_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "state"
     t.boolean  "seen",       default: false
     t.text     "review"
     t.integer  "stars"
@@ -86,7 +85,6 @@ ActiveRecord::Schema.define(version: 20150406164741) do
 
   add_index "favorite_movies", ["movie_id"], name: "index_favorite_movies_on_movie_id"
   add_index "favorite_movies", ["seen"], name: "index_favorite_movies_on_seen"
-  add_index "favorite_movies", ["state"], name: "index_favorite_movies_on_state"
   add_index "favorite_movies", ["user_id"], name: "index_favorite_movies_on_user_id"
 
   create_table "favorite_theaters", force: true do |t|
@@ -98,6 +96,13 @@ ActiveRecord::Schema.define(version: 20150406164741) do
 
   add_index "favorite_theaters", ["theater_id"], name: "index_favorite_theaters_on_theater_id"
   add_index "favorite_theaters", ["user_id"], name: "index_favorite_theaters_on_user_id"
+
+  create_table "friendships", force: true do |t|
+    t.integer  "user_id"
+    t.string   "uid"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "genres", force: true do |t|
     t.string   "name"
