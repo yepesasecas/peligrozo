f837d95ba3856fafb62e7136fc857deb33036fa3
Andres Yepes <yepes07@gmail.com>
2015-05-11 14:09:37 -0500

    remove update_genre from controller

--- app/controllers/upcoming_movies_controller.rb
+++ app/controllers/upcoming_movies_controller.rb
@@ -1,8 +1,6 @@
 class UpcomingMoviesController < ApplicationController
   def show
     movie = Movie.find_or_create_by(tmdb_id: params[:id])
-    movie.update_genres
-    
     @movie_decorator = MovieDecorator.new(movie, current_user)
   end
 end
