5a2e8e4c8b56a440f54c5034db20e07f2336e3c3
Andres Yepes <yepes07@gmail.com>
2015-06-17 10:17:15 -0500

    add Exceptions to MovieDetails

--- app/models/concerns/movie_details.rb
+++ app/models/concerns/movie_details.rb
@@ -2,20 +2,30 @@ module MovieDetails
   extend ActiveSupport::Concerns
 
   def update_genres
-    return unless tmdb_id.present?
-    if genres = Fetch::Moviesdb.detail(tmdb_id).genres
-      genres.each{|genre| self.movie_genres.create(genre: Genre.find_by(tmdb_id: genre["id"]))}
+    begin
+      return unless tmdb_id.present?
+      if genres = Fetch::Moviesdb.detail(tmdb_id).genres
+        genres.each{|genre| self.movie_genres.create(genre: Genre.find_by(tmdb_id: genre["id"]))}
+      end
+    rescue Exception => e
+      p "##################"
+      p "ERROR update_genres: #{e.message}"
     end
   end
 
   private
 
     def get_details
-      get_tmdb_id_by_name if tmdb_id.nil?
+      begin
+        get_tmdb_id_by_name if tmdb_id.nil?
 
-      if tmdb_id.present?
-        get_details_by_id
-        get_trailer unless trailer.present?
+        if tmdb_id.present?
+          get_details_by_id
+          get_trailer unless trailer.present?
+        end
+      rescue Exception => e
+        p "##################"
+        p "ERROR get_details: #{e.message}"
       end
     end
 
